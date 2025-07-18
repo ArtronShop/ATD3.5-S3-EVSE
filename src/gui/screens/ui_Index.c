96b66fe6612695a0a9c4333eeabc654e99a8d2d4
Max <7087907+maxpromer@users.noreply.github.com>
2025-07-19 00:07:56 +0700

    Update UI

--- src/gui/screens/ui_Index.c
+++ src/gui/screens/ui_Index.c
@@ -173,7 +173,7 @@ void ui_Index_screen_init(void)
     lv_obj_set_width(ui_Label2, LV_SIZE_CONTENT);   /// 1
     lv_obj_set_height(ui_Label2, LV_SIZE_CONTENT);    /// 1
     lv_obj_set_align(ui_Label2, LV_ALIGN_CENTER);
-    lv_label_set_text(ui_Label2, "Power (W)");
+    lv_label_set_text(ui_Label2, "Power (kW)");
     lv_obj_set_style_text_color(ui_Label2, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_set_style_text_opa(ui_Label2, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_set_style_text_font(ui_Label2, &lv_font_montserrat_14, LV_PART_MAIN | LV_STATE_DEFAULT);
